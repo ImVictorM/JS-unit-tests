be07c4e69a4ff6fd9c51e5089377020273e9ab02
Victor Mendes <victorutdsim7@gmail.com>
2022-07-19 19:12:16 -0300

    [REQ 8] muda o nome da variavel do laço interior da funcao myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,11 +19,12 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var repeater = 2; repeater <= 3; repeater += 1) {
+      myArray.push(repeater);
     }
   }
   return myArray;
 };
-
+//const expectedOutput = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+console.log(myCounter());
 module.exports = myCounter;
